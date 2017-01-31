b4805ec0d9c608d80c63667c29a6297a76e400c0
Thomas Allan Lamb <lambyte@lambyte.com>
2017-01-30 21:01:59 -0800

    data displays as slices of n

--- generate.py
+++ generate.py
@@ -49,33 +49,43 @@ def recall (m, n, u):
 
 
 def mnu (m, n, u):
+  print 'mnu('+str(m)+','+str(n)+','+str(u)+')'
 
   if isStored(m, n, u):
     return recall(m, n, u)
 
-  #we don't already have the answer.
+  #use native operators when possible
+  elif n == 1:
+    #addition
+    result = m+u-(identity(n))
+  #???: can standard multiplication be used when i(1) !== 0?
+  elif (n == 2 and (identity(1) == 0)): 
+    #multiplication
+    result = m*(u-identity(2)+1)
+  elif (n == 3 and identity(1) == 0 and identity(2) == 1):
+    #exponentiation
+    result = u**(m-identity(3)+1)
+
   else:
     #solve using definition
-
     if identity(n) == m:
       ##print "identity"
       # if m is the identity of n, return u
       result = u
-      store(m,n,u,result)
-      return result
 
     elif n == 1:
-      ##print "addition"
+      #print "addition"
       # addition.
       result = u+m
-      store(m,n,u,result)
-      return result
 
     else:
-      ##print "recurse"
+      #print "recurse"
       result = mnu(mnu(m-1, n, u), n-1, u)
-      store(m,n,u,result)
-      return result
+
+  #print 'store('+str(m)+','+str(n)+','+str(u)+','+str(result)+')'
+
+  store(m,n,u,result)
+  return result
 
 with open('public/standard.csv', 'w') as csvFile:
   csvWriter = csv.writer(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
@@ -87,6 +97,7 @@ with open('public/standard.csv', 'w') as csvFile:
     for m in range(identity(n), boundary['m']+1):
       #inputs where u=0 such as 1,3,0 will eventually become 0,3,0. But we have disallowed m=0. So instead of starting at u=0, start at u=identity
       for u in range(identity(n), boundary['u']+1):
+        #print '---'
         output = [m, n, u, mnu(m, n, u)]
         csvWriter.writerow(output)
 
--- public/javascript/linechart.js
+++ public/javascript/linechart.js
@@ -1,59 +1,104 @@
 $(document).ready(function () {
-  var svg = d3.select("svg");
-  var margin = {top: 20, right: 20, bottom: 30, left: 50};
-  var width = +svg.attr("width") - margin.left - margin.right;
-  var height = +svg.attr("height") - margin.top - margin.bottom;
-  var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  //intended size of svgs in px
+  const svgDim = {width:960, height:500};
 
-  var parseTime = d3.timeParse("%d-%b-%y");
-
-  var x = d3.scaleLinear()
-      .rangeRound([0, width]);
-
-  var y = d3.scaleLinear()
-      .rangeRound([height, 0]);
-
-  var line = d3.line()
-      .x(function(d) { return x(d.u); })
-      .y(function(d) { return y(d.r); });
-
+  //request the data
   d3.csv(
     "standard.csv"
-    , function(d) {
-      //do not modify rows
-      return d;
+    , function(row) {
+      //each cell is in string format, so convert to numbers
+      return {
+        m: +row.m,
+        n: +row.n,
+        u: +row.u,
+        r: +row.r
+      }
     }
     , function(error, data) {
       if (error) throw error;
 
-      x.domain(d3.extent(data, function(d) { return d.u; }));
-      y.domain(d3.extent(data, function(d) { return d.r; }));
+      var categorizedData = d3.nest()
+        .key(function (d) {return d.n})
+        .key(function (d) {return d.u})
+        .entries(data)
+      ;
 
-      g.append("g")
+      for (let nCategory of categorizedData)
+      {
+        $("body").append("<p>n = "+nCategory.key+"</p>");
+        var svg = d3.select("body").append('svg')
+          .attr('width', svgDim.width)
+          .attr('height', svgDim.height)
+        ;
+
+        var margin = {top: 20, right: 20, bottom: 30, left: 50};
+        var width = +svg.attr("width") - margin.left - margin.right;
+        var height = +svg.attr("height") - margin.top - margin.bottom;
+        var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var x = d3.scaleLinear()
+          .rangeRound([0, width])
+        ;
+        var y = d3.scaleLinear()
+          .rangeRound([height, 0])
+        ;
+
+        var concatenatedUData = data.filter(function (v) {return v.n === parseInt(nCategory.key, 10)});
+
+        x.domain(
+          d3.extent(
+            concatenatedUData
+            , function (d) {return d.m}
+          )
+        );
+        y.domain(
+          d3.extent(
+            concatenatedUData
+            , function (d) {return d.r}
+          )
+        );
+
+        g.append("g")
           .attr("transform", "translate(0," + height + ")")
           .call(d3.axisBottom(x))
-        .select(".domain")
-          .remove();
+        ;
 
-      g.append("g")
+        g.append("g")
           .call(d3.axisLeft(y))
-        .append("text")
+          .append("text")
           .attr("fill", "#000")
           .attr("transform", "rotate(-90)")
           .attr("y", 6)
           .attr("dy", "0.71em")
           .attr("text-anchor", "end")
-          .text("m");
+          .text("m")
+        ;
 
-      debugger;
-      g.append("path")
-          .datum(data)
-          .attr("fill", "none")
-          .attr("stroke", "steelblue")
-          .attr("stroke-linejoin", "round")
-          .attr("stroke-linecap", "round")
-          .attr("stroke-width", 1.5)
-          .attr("d", line);
+        for (let uCategory of nCategory.values)
+        {
+          
+          var line = d3.line()
+            .x(function(d) { return x(d.m); })
+            .y(function(d) { return y(d.r); })
+          ;
+          
+          //generate a random color for to differentiate each u category
+          var colorString = '#'
+            +Math.floor(Math.random()*100+100).toString(16)
+            +Math.floor(Math.random()*100+100).toString(16)
+            +Math.floor(Math.random()*100+100).toString(16)
+          ;
+          g.append("path")
+            .datum(uCategory.values)
+            .attr("fill", "none")
+            .attr("stroke", colorString)
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-linecap", "round")
+            .attr("stroke-width", 2)
+            .attr("d", line)
+          ;
+        }
+      }
     }
   );
 });
